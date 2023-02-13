bb48fdfa5b5f3da77e9a469952096ac1915d2080
SaagarSuresh <saagarsuresh92@gmail.com>
2023-02-13 18:42:01 -0500

    Create HomePage.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 import PageBar from './components/PageBar/PageBar';
+import HomePage from './pages/HomePage/HomePage';
 import StorePage from './pages/StorePage/StorePage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<HomePage/>} />
       <Route path="/store" element={<StorePage/>} />
     </Routes>
     </BrowserRouter>
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+import "./HomePage.scss";
+
+export default function HomePage(){
+    return(
+        <section className="homepage">
+            <Header/>
+            <h2>Welcome to 3E-nventory!</h2>
+        </section>
+    )
+}
